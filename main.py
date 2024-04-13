11975c5a7196117d5d29b9d8112422be2267a7f7
Martin Jochum <mjochum64@gmail.com>
2024-04-13 12:12:18 +0200

    optimize

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import time
 import json
 import os
 from threading import Thread
+import uvicorn
 from fastapi import FastAPI
 from status_module import router as status_router
 from cargo_module import router as cargo_router
 from log_module import router as log_router, get_current_star_system
 
-app = FastAPI(title="EliteStatusCheck v0.1.0")
+app = FastAPI(
+    title="Elite Status Check",
+    version="0.0.1",
+    description="Eine API zum Überwachen von Elite Dangerous Statusdaten",
+)
 
 # Dateinamen, die überwacht werden sollen
 files_to_watch = ["Status.json", "Cargo.json"]
+
 # Pfad zum Verzeichnis, in dem die Dateien liegen
-base_path = os.path.join(os.environ['USERPROFILE'], 'Saved Games', 'Frontier Developments', 'Elite Dangerous')
+base_path = os.path.join(
+    os.environ["USERPROFILE"], "Saved Games", "Frontier Developments", "Elite Dangerous"
+)
 
 # Caching der Dateiinhalte
 cached_data = {}
 
 def read_and_update_files():
+    """
+    Reads and updates files based on the content of files_to_watch.
+    """
     for filename in files_to_watch:
         file_path = os.path.join(base_path, filename)
         try:
@@ -28,17 +39,20 @@ def read_and_update_files():
             print(f"Fehler beim Lesen von {filename}: {e}")
 
 def start_file_watcher():
+    """
+    Start the file watcher that reads and updates specified files every 10 seconds.
+    """
     while True:
         read_and_update_files()
         time.sleep(10)  # Überprüfe die Dateien alle 10 Sekunden
 
-def start_watcher_thread():
-    watcher_thread = Thread(target=start_file_watcher, daemon=True)
-    watcher_thread.start()
-
 @app.on_event("startup")
 async def startup_event():
-    start_watcher_thread()
+    """
+    Handles the startup event of the FastAPI application by starting the file watcher thread.
+    """
+    watcher_thread = Thread(target=start_file_watcher, daemon=True)
+    watcher_thread.start()
 
 # Module einbinden
 app.include_router(status_router, prefix="/status")
@@ -47,13 +61,21 @@ app.include_router(log_router, prefix="/logs")
 
 @app.get("/")
 async def read_root():
+    """
+    A function to retrieve the current star system, flags, and flags2 from status data.
+    """
     # Hole das aktuelle Sternensystem vom log_module
     current_star_system = await get_current_star_system()
     system_name = current_star_system.get("StarSystem", "Unbekanntes System")
-    
+
     # Hole die Flags und Flags2 aus der Status.json, falls verfügbar
     status_data = cached_data.get("Status.json", {})
     flags = status_data.get("Flags", 0)
     flags2 = status_data.get("Flags2", 0)
-    
+
     return {"System": system_name, "Flags": flags, "Flags2": flags2}
+
+
+# Start the FastAPI application
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8888)
